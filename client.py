ff4168d5da24a223b1e105112291cca8e2f72e48
luke12harris <luke12harris@gmail.com>
2022-05-28 18:08:53 +0100

    changed client messages

--- client.py
+++ client.py
@@ -1,16 +1,27 @@
 import socket
 
-HOST = '192.168.0.7'
-PORT = 9090
+def main();
+    HOST = '192.168.0.7'
+    PORT = 9090
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-socket.connect((HOST, PORT))
+    socket.connect((HOST, PORT))
 
-message = input("Enter message to send: ")
+    while True:
+        message = input("Enter message to send: ")
+        socket.send(message.encode('utf-8'))
 
-socket.send(message.encode('utf-8'))
+        data = socket.recv(1024)
 
-print(socket.recv(1024).decode('utf-8'))
+        print(f"Server recieved: {str(data.decode('utf-8'))} ")
 
-input()
+        reply = input("Reply? 0/1")
+        if reply = 0:
+            break
+    socket.close
+
+    input()
+
+if __name__ =="__main__":
+    main()
